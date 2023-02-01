115993a398ba35dabb828e589377ca63b6d0f25a
tathagatamishra <tathagatamishra007@gmail.com>
2023-02-01 17:21:39 +0530

    debug

--- click.js
+++ click.js
@@ -7,15 +7,16 @@ function eye() {
 
         document.getElementById('one').style.zIndex='2'
         document.getElementById('two').style.zIndex='2'
-        
+        console.log('gg');
         return a=0
     }
     
     else {
-
+        
         document.getElementById('one').style.zIndex='-1'
         document.getElementById('two').style.zIndex='-1'
-
+        
+        console.log('wp');
         return a=1
     }
 }
