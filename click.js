12b70f98fd6689d2740e4edb7351737e607a667f
tathagatamishra <tathagatamishra007@gmail.com>
2023-02-01 16:19:04 +0530

    fixing bugs

--- click.js
+++ click.js
@@ -1,13 +1,12 @@
 
 let a
 
-function show_hide() {
+function eye() {
 
     if (a==1) {
 
         document.getElementById('one').style.zIndex='2'
         document.getElementById('two').style.zIndex='2'
-        document.getElementById('three').style.zIndex='2'
         
         return a=0
     }
@@ -16,7 +15,7 @@ function show_hide() {
 
         document.getElementById('one').style.zIndex='0'
         document.getElementById('two').style.zIndex='0'
-        document.getElementById('three').style.zIndex='0'
+
 
         return a=1
     }
